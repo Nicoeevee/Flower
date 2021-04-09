a3c1f8085ab652cc1b424c6d528bb578e2100b9c
Apo <maoexon@gmail.com>
2021-04-09 22:50:50 +0800

    Add OnInteractionUsed StatusString support

--- Source/FlowQuest/Private/Flow/Nodes/FlowNode_OnInteractionUsed.cpp
+++ Source/FlowQuest/Private/Flow/Nodes/FlowNode_OnInteractionUsed.cpp
@@ -14,11 +14,11 @@ void UFlowNode_OnInteractionUsed::ObserveActor(TWeakObjectPtr<AActor> Actor, TWe
 	{
 		TArray<UInteractionComponent*> FoundInteractions;
 		Actor->GetComponents<UInteractionComponent>(FoundInteractions);
-		
+
 		if (FoundInteractions.Num() > 0)
 		{
 			RegisteredActors.Emplace(Actor, Component);
-			
+
 			ObservedInteractions.Emplace(Actor, FoundInteractions[0]);
 			FoundInteractions[0]->OnUsed.AddDynamic(this, &UFlowNode_OnInteractionUsed::OnEventReceived);
 		}
@@ -29,8 +29,22 @@ void UFlowNode_OnInteractionUsed::ForgetActor(TWeakObjectPtr<AActor> Actor, TWea
 {
 	ensureAlways(ObservedInteractions.Contains(Component->GetOwner()));
 	TWeakObjectPtr<UInteractionComponent> InteractionComponent = ObservedInteractions[Component->GetOwner()];
-	
+
 	InteractionComponent->OnUsed.RemoveAll(this);
+}
+
+FString UFlowNode_OnInteractionUsed::GetInteractionProgress() const
+{
+	if (ObservedInteractions.Num())
+	{
+		return GetProgressAsString(SuccessCount) + TEXT(" / ") + GetProgressAsString(SuccessLimit);
+	}
+	return FString();
+}
+
+FString UFlowNode_OnInteractionUsed::GetStatusString() const
+{
+	return GetInteractionProgress();
 }
 
 void UFlowNode_OnInteractionUsed::Cleanup()
--- Source/FlowQuest/Public/Flow/Nodes/FlowNode_OnInteractionUsed.h
+++ Source/FlowQuest/Public/Flow/Nodes/FlowNode_OnInteractionUsed.h
@@ -13,14 +13,21 @@ UCLASS(NotBlueprintable, meta = (DisplayName = "On Interaction Used"))
 class FLOWQUEST_API UFlowNode_OnInteractionUsed : public UFlowNode_ComponentObserver
 {
 	GENERATED_UCLASS_BODY()
-	
+
 private:
 	TMap<TWeakObjectPtr<AActor>, TWeakObjectPtr<UInteractionComponent>> ObservedInteractions;
 
 protected:
 	virtual void ObserveActor(TWeakObjectPtr<AActor> Actor, TWeakObjectPtr<UFlowComponent> Component) override;
 	virtual void ForgetActor(TWeakObjectPtr<AActor> Actor, TWeakObjectPtr<UFlowComponent> Component) override;
-	
+
+public:
+	FString GetInteractionProgress() const;
+
+#if WITH_EDITOR
+	virtual FString GetStatusString() const override;
+#endif
+
 protected:
 	virtual void Cleanup() override;
 };
